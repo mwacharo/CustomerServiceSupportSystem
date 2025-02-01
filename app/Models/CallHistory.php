6ff63f835b41df63754c96ee6ff729cd4488aab7
mwacharo <mwacharommwanyolo@gmail.com>
2025-02-01 07:34:00 +0300

    Added fillable properties for CallHistory

--- app/Models/CallHistory.php
+++ app/Models/CallHistory.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class CallHistory extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'isActive',
+        'direction',
+        'sessionId',
+        'callerNumber',
+        'destinationNumber',
+        'durationInSeconds',
+        'currencyCode',
+        'recordingUrl',
+        'amount',
+        'hangupCause',
+        'adminId',
+        'agentId',
+        'orderNo',
+        'notes',
+        'nextCallStep',
+        'conference',
+        'status',
+    ];
 }
