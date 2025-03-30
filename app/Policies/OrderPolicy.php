7257f6fcfd0d389ab8698c5de5011ae636266ead
mwacharo <mwacharommwanyolo@gmail.com>
2025-03-30 11:44:18 +0300

    commit

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Property;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class PropertyPolicy
+class OrderPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class PropertyPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Property $property): bool
+    public function view(User $user, Order $order): bool
     {
         //
     }
@@ -35,7 +35,7 @@ class PropertyPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Property $property): bool
+    public function update(User $user, Order $order): bool
     {
         //
     }
@@ -43,7 +43,7 @@ class PropertyPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Property $property): bool
+    public function delete(User $user, Order $order): bool
     {
         //
     }
@@ -51,7 +51,7 @@ class PropertyPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Property $property): bool
+    public function restore(User $user, Order $order): bool
     {
         //
     }
@@ -59,7 +59,7 @@ class PropertyPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Property $property): bool
+    public function forceDelete(User $user, Order $order): bool
     {
         //
     }
