63eb5750cd2a7d80b5d167ce8f5d5a83eb7715a5
mwacharo <mwacharommwanyolo@gmail.com>
2025-03-30 08:48:37 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -87,7 +87,7 @@ Route::apiResource('v1/branches', ApiBranchController::class)->only([
 Route::post('v1/africastalking-handle-callback', [ApiCallCentreController::class, 'handleVoiceCallback']);
 Route::post('v1/africastalking-handle-event', [ApiCallCentreController::class, 'handleEventCallback']);
 Route::get('v1/voice-token', [ApiCallCentreController::class, 'generateToken']);
-Route::post('v1/call-history', [ApiCallCentreController::class, 'fetchCallHistory']);
+Route::get('v1/call-history', [ApiCallCentreController::class, 'fetchCallHistory']);
 
 // Route::post('/africastalking-handle-selection', [ApiCallCentreController::class, 'handleSelection']);
 // make a new call
