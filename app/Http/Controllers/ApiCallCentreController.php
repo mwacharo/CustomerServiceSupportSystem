826bf9e23b042c9160dffe0382e5bbac2d51924d
mwacharo <mwacharommwanyolo@gmail.com>
2025-04-04 06:17:15 +0300

    commit

--- app/Http/Controllers/ApiCallCentreController.php
+++ app/Http/Controllers/ApiCallCentreController.php
@@ -783,20 +783,22 @@ class ApiCallCentreController extends Controller
 
 
     private function recordVoicemail()
-{
-
-     Log::info("Recording voicemail...");
-    // Compose the XML response
-    $response  = '<?xml version="1.0" encoding="UTF-8"?>\n<Response>\n';
-    $response .= '<Response>';
-    $response .= '<Say playBeep="true">Please leave a message after the tone.</Say>';
-    $response .= '<Record finishOnKey="#" maxLength="30" playBeep="true" callbackUrl="https://support.solssa.com/api/v1/africastalking-handle-event"/>';
-    $response .= '<Say>Thank you for your message. We will get back to you soon.</Say>';
-    $response .= '</Response>';
-
-    // Return XML response
-    return response($response, 200)->header('Content-Type', 'text/xml');
-}
+    {
+        Log::info("Recording voicemail...");
+    
+        // Compose the XML response
+        $xmlResponse = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xmlResponse .= '<Response>';
+        $xmlResponse .= '<Say playBeep="true">Please leave a message after the tone.</Say>';
+        $xmlResponse .= '<Record finishOnKey="#" maxLength="30" playBeep="true" callbackUrl="https://support.solssa.com/api/v1/africastalking-handle-event"></Record>';
+        $xmlResponse .= '<Say>Thank you for your message. We will get back to you soon.</Say>';
+        $xmlResponse .= '</Response>';
+    
+        // Return XML response
+        return response($xmlResponse, 200)
+            ->header('Content-Type', 'application/xml'); // Use 'application/xml'
+    }
+    
 
 
 
