00f7fc73cb7c5ed1a741d8585c915b6fd8a6b2cc
mwacharo <mwacharommwanyolo@gmail.com>
2025-04-26 05:04:25 +0200

    commit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -150,6 +150,11 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
     // public function  vendor()
     // {
     //     return $this->belongsTo(Vendor::class);
